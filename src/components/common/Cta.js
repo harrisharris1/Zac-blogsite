b09ebbf3525410187ec9d95956e8d1d6bf659539
Andrea Harris <andreaharris@andreas-mbp.lan>
2021-04-20 13:43:06 -0500

    modified Layout, is working

--- src/components/common/Cta.js
+++ src/components/common/Cta.js
@@ -31,3 +31,4 @@ const CtaQuote = ()=>(
         </div> 
         </div>
         )
+        export default CtaQuote
--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -5,7 +5,11 @@ import { Link, StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import { Navigation } from '.'
+import {Header} from ".";
+import {Main} from ".";
+import {CtaQuote} from ".";
 import config from '../../utils/siteConfig'
+import Heart from "../../images/heart.svg"
 
 // Styles
 import '../../styles/app.css'
@@ -32,65 +36,42 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
             </Helmet>
 
             <div className="viewport">
-
-                <div className="viewport-top">
-                    {/* The main header section on top of the screen */}
-                    <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
-                        <div className="container">
-                            <div className="site-mast">
-                                <div className="site-mast-left">
-                                    <Link to="/">
-                                        {site.logo ?
-                                            <img className="site-logo" src={site.logo} alt={site.title} />
-                                            : <Img fixed={data.file.childImageSharp.fixed} alt={site.title} />
-                                        }
-                                    </Link>
-                                </div>
-                                <div className="site-mast-right">
-                                    { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                    { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
-                                    <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
-                                </div>
-                            </div>
-                            { isHome ?
-                                <div className="site-banner">
-                                    <h1 className="site-banner-title">{site.title}</h1>
-                                    <p className="site-banner-desc">{site.description}</p>
-                                </div> :
-                                null}
-                            <nav className="site-nav">
-                                <div className="site-nav-left">
-                                    {/* The navigation items as setup in Ghost */}
-                                    <Navigation data={site.navigation} navClass="site-nav-item" />
-                                </div>
-                                <div className="site-nav-right">
-                                    <Link className="site-nav-button" to="/about">About</Link>
-                                </div>
-                            </nav>
+                <header>
+                    <div className="container">
+                        <div className="site-mast">
+                         <Navigation/>
                         </div>
-                    </header>
 
-                    <main className="site-main">
+                        <div className="site-banner-header">
+                         <Header/>
+                        </div>
+
+                        <div className="site-main-about">
+                         <Main/>
+                         <CtaQuote/>
+                         </div>
+
+                    </div>
+                </header>
+
+                <main className="site-main">
                         {/* All the main content gets inserted here, index.js, post.js */}
                         {children}
-                    </main>
+                </main>
+            </div>
 
-                </div>
-
-                <div className="viewport-bottom">
-                    {/* The footer at the very bottom of the screen */}
-                    <footer className="site-foot">
-                        <div className="site-foot-nav container">
-                            <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2021 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
-                            </div>
-                            <div className="site-foot-nav-right">
-                                <Navigation data={site.navigation} navClass="site-foot-nav-item" />
-                            </div>
+            <div className="viewport-bottom">
+                <footer className="site-foot">
+                    <div className="site-foot-nav container">
+                        <div className="site-foot-nav-left">
+                          <img src={Heart}></img>
+                          <h3>Made with love by zac harris</h3>
                         </div>
-                    </footer>
 
-                </div>
+                    </div>
+
+                </footer>
+
             </div>
 
         </>
